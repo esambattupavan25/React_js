bc9a00b082a0a4d8948a2ffa3945004d4606391d
esambattupavan25 <esambattupavan25@gmail.c0m>
2024-04-10 13:28:53 +0530

    Star-Rating

--- project1/src/App.js
+++ project1/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Accordian } from './Components/Accordion/User';
 import Randomcolor from './Components/random colors';
+import StarRating from './Components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian/> */}
-      <Randomcolor/>
+      {/* <Randomcolor/> */}
+      {/* <StarRating/> */}
 
     </div>
   );
---
+++ project1/src/Components/image-slider/index.jsx
@@ -0,0 +1,9 @@
+
+
+
+export default function ImageSlider{{URL,limit}
+   return <div className="container">
+
+   </div>
+ 
+}
