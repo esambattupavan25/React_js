3ef30dc826ec96453314b9eb4164eff6411f49ac
esambattupavan25 <esambattupavan25@gmail.c0m>
2024-04-11 10:59:33 +0530

    added

--- project1/src/App.js
+++ project1/src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 import { Accordian } from './Components/Accordion/User';
 import Randomcolor from './Components/random colors';
 import StarRating from './Components/star-rating';
+import ImageSlider from './Components/image-slider';
+import QRCode from 'react-qr-code';
+import QRCodeGenerator from './Components/QR-Code-Generator';
+import ModelTest from './Components/Custom-Model-Popup/modal-test';
 
 function App() {
   return (
     <div className="App">
+      {/* Project 1 */}
       {/* <Accordian/> */}
+
+      {/* Project 2 */}
       {/* <Randomcolor/> */}
+
+      {/* Project 3 */}
       {/* <StarRating/> */}
 
+      {/* Project 4 */}
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} /> */}
+
+      {/* Project 5 */}
+      {/* <QRCodeGenerator/> */}
+
+      {/* Project 6 */}
+      {/* <ModelTest/> */}
     </div>
   );
 }
